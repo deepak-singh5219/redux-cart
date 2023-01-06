b65ee5314eeb5bb1dde31a92b25fd09bb62ac942
Deepak Singh <singhdeepak5219@gmail.com>
2023-01-06 23:35:27 +0530

    redux store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Auth from './components/Auth/auth';
+import Layout from './components/Layout/layout';
 
 const App = () => {
   return (
     <div>
       <Auth/>
+      {/* <Layout/> */}
     </div>
   )
 }
